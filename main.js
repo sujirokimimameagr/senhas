006c02d9a9683d1cd2afa8034a6bf5e525181d20
kuster <kuster.matheus@escola.pr.gov.br>
2024-07-24 16:16:37 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -19,6 +19,60 @@ function diminuiTamanho() {
         tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
-    
-    ()
+    geraSenha();
 }
+function aumentaTamanho(){
+    if(tamanhosenha <20 ){
+        //tamanhoSenha = tamanhoSenha+1;
+        tamanhoSenha++;
+    }
+    numeroSenha.textContent=tamanhoSenha;
+    geraSenha()
+}
+
+for(i = 0; i < checkbox.length;i++) {
+    checkbox[i].onclick = geraSenha;
+}
+
+geraSenha();
+
+function geraSenha(){
+    let alfabeto ='';
+    if(checkbox [0]. checked) {
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+    if(checkbox [1]. checked) {
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+    if(checkbox [2]. checked) {
+        alfabeto = alfabeto + numeros;
+    }
+    if(checkbox [3]. checked) {
+        alfabeto = alfabeto + simbolos;
+    }
+}
+ for (let i=0; i < tamanhosenha; i++){
+    let numeroAleatorio = Math.random()* alfabeto.length;
+    numeroAleatorio = Math.floor(numeroAleatorio);
+    senha = senha + alfabeto[numeroAleatorio];
+ } 
+ campoSenha.value = senha;
+ classificaSenha(alfabeto.length);
+  
+}
+
+function  classificaSenha(tamanhoAlfabeto){
+    let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
+    console.log(entropia);
+    forcaSenha.classList.remove('fraca','media','forte');
+    if(entropia >57){
+        forcaSenha.classList.add('forte');
+    } else if (entropia > 35 && entropia < 57) {
+        forcaSenha.classList.add('media');
+    } else if ( entropia <= 35) {
+        forcaSenha.classList.add('fraca');
+    }
+    const valorEntropia = document.querySelector('.entropia');
+    valorEntropia.textContent = “um computador pode levar até” +Math.floor(2**entropia/(100e6*60*60*24)) + “ dias para descobrir esse senha”;
+          }
+
